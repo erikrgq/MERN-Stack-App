13dc291b1b9c1f22c24d13bd47e7513a55d6991f
egardea <erik1gardea@gmail.com>
2020-04-26 22:31:41 -0500

    server setup and first route/schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    calories: {
+        type: Number,
+        required: true
+    },
+    caloriegoal: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
---
+++ backend/routes/user.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+    User.find()
+    .then(users => res.json(users))
+    .catch(error => res.status(400).json('Error: '+error));
+});
+
+router.route('/add').post((req, res) => {
+    const user = req.body.user;
+
+    const newUser = new User({user});
+
+    newUser.save()
+    .then(() => res.json('user added'))
+    .catch(error => res.status(400).json('error: ' + error));
+});
+
+export default router;
