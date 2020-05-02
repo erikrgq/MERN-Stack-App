9e7173a258d2e4894a556f5c870bf3e920e53556
egardea <erik1gardea@gmail.com>
2020-05-02 12:22:05 -0500

    added comments

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,20 +1,26 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// Retrieves all the users
 router.route('/').get((req, res) => {
+
     User.find()
-    .then(users => res.json(users))
-    .catch(error => res.status(400).json('Error: '+error));
+        .then(users => res.json(users))
+        .catch(error => res.status(400).json('Error: '+error));
+
 });
 
+// Adds new a new user
 router.route('/add').post((req, res) => {
+
     const user = req.body.user;
 
     const newUser = new User({user});
 
     newUser.save()
-    .then(() => res.json('user added'))
-    .catch(error => res.status(400).json('error: ' + error));
+        .then(() => res.json('user added'))
+        .catch(error => res.status(400).json('error: ' + error));
+
 });
 
 module.exports = router;
