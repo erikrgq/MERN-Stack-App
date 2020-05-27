ca410f2398b3df757c9eeb8fe4ccb3aa61e37106
egardea <erik1gardea@gmail.com>
2020-05-26 20:54:44 -0500

    Refactored the get and put routes

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -5,10 +5,16 @@ const User = require('../models/user.model');
 // retrieves all of the food 
 router.route('/').get((req, res) => {
 
-    const userID = req.body.id;
+    const foodList =  [
+        "5ecdb1d7973c901b84afb372",
+        "5ecdc4c4c2f6dc381022b1fd",
+        "5ecdc5840a66cf4ed091d3a8",
+        "5ecdc5950a66cf4ed091d3a9",
+        "5ecdc5bf0a66cf4ed091d3aa"
+    ];
 
-    User.findById(userID)
-        .then(user => res.json(user.food))
+    Food.find().where('_id').in(foodList).exec()
+        .then(foodsFound => res.json(foodsFound))
         .catch(err => res.statusCode(400).json(err));
 
 });
@@ -16,30 +22,38 @@ router.route('/').get((req, res) => {
 // add a food item to the databse
 router.route('/add').put((req, res) => {
 
-    const userID = req.body.id;
+    try {
+        const userID = req.body.id;
 
-    const newFood = new Food({
-        calories: req.body.calories,
-        ingredients: req.body.ingredients,
-        carbs: req.body.carbs,
-        protein: req.body.protein,
-        fat: req.body.fat,
-        servings: req.body.servings,
-        servingmeasurement: req.body.servingmeasurement,
-        date: req.body.date
-    });
+        const newFood = new Food({
+            calories: req.body.calories,
+            ingredients: req.body.ingredients,
+            carbs: req.body.carbs,
+            protein: req.body.protein,
+            fat: req.body.fat,
+            servings: req.body.servings,
+            servingmeasurement: req.body.servingmeasurement,
+            date: req.body.date
+        });
 
-    User.updateOne(
-        {_id: userID},
-        {$push: {food: newFood}},
-        function(error, result) {
-            if(error) {
-                res.send(error);
-            } else {
-                res.send(result);
+        newFood.save()
+        .then(() => res.json('food added'))
+        .catch(error => res.status(400).json('error: ' + error));
+
+        User.updateOne(
+            {_id: userID},
+            {$push: {food: newFood._id}},
+            function(error, result) {
+                if(error) {
+                    res.send(error);
+                } else {
+                    res.send(result);
+                }
             }
-        }
-    );
+        );
+    } catch (err) {
+        res.send(err);
+    }
 
 });
 
