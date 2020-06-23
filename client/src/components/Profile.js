a00ed2adf51d786faafac65089c1dd5250d7fae7
egardea <erik1gardea@gmail.com>
2020-06-22 19:51:40 -0500

    Added state and HTTP post method to add the user to the data base

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Profile() {
+
+    const [calorieGoal, setCalorieGoal] = useState();
+    const [username, setUsername] = useState('');
+
+    const saveUser = ()  => {
+
+        const data = {
+            user: username,
+            calories: 0,
+            caloriegoal: calorieGoal
+
+        }
+
+        fetch('http://localhost:5000/user/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'Application/json'
+                },
+                body: JSON.stringify(data)
+        })
+            .then(res => res.json(res))
+            .catch(err => console.log(err))
+
+    }
+
+    const setUserState = (e) => {
+        let user = e.target.value;
+        setUsername(user);
+    }
+
+    const setCalorieState = (e) => {
+        e.preventDefault();
+        let calories = parseInt(e.target.value);
+        setCalorieGoal(calories);
+    }
 
     return (
         <div id="profile">
@@ -15,14 +50,14 @@ export default function Profile() {
                     <section className="card">
                         <div>
                             <p>Calorie Goal</p>
-                            <input placeholder="Enter Calorie Goal" />
+                            <input placeholder="Enter Calorie Goal" type="number" onChange={setCalorieState} />
                         </div>
                         <div>
                             <p>Username</p>
-                            <input placeholder="Enter Username" />
+                            <input placeholder="Enter Username" type="text" onChange={setUserState} />
                         </div>
                         <div>
-                            <button className="btn">Submit</button>
+                            <button className="btn" onClick={saveUser}>Submit</button>
                         </div>
                     </section>
                 </div>
