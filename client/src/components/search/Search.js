41dbe751340aa85d6b64c4422ea58426ba96da17
egardea <erik1gardea@gmail.com>
2020-06-22 23:32:32 -0500

    Made the first GET call to the USDA api for nutritional data

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchCard from './Search.Card';
 
 export default function Search() {
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const getSearchResults = () => {
+
+        if(searchQuery) {
+
+            fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DHceyyoJMX48paCbIw9wJfvy7JWNDUacApePfS4I&query=${searchQuery}`)
+                .then(res => res.json(res))
+                .then(res => setSearchResults(res.foods))
+                .catch(err => console.log(err)); 
+        }
+
+    }
+
+    const setSearchState = (e) => {
+        
+        e.preventDefault()
+        let query = e.target.value;
+        setSearchQuery(query);
+
+    }
+
     return (
         <div id="search">
             <main  className="content">
@@ -10,21 +34,23 @@ export default function Search() {
                         <p>Search</p>
                     </div>
                     <div className="search">
-                        <input placeholder="Search Food" />
-                        <button className="btn">Search</button>
+                        <input placeholder="Search Food" type="text" onChange={setSearchState} />
+                        <button className="btn" onClick={getSearchResults} >Search</button>
                     </div>
                 </section>
                 <section className="content">
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
+
+                    {
+                        searchResults.length > 0 ?
+                        searchResults.map((cur) => (
+
+                            <SearchCard key={cur.fdcId} food={cur} />
+
+                        ))
+                        :
+                        ''
+                    }
+
                 </section>
             </main>
         </div>
